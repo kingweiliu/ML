bddf841681c0c9bffe5807d87896fb3100ef4092
kwl <kingweiliu@gmail.com>
2013-12-21 21:06:07 +0800

    kNN first

---
+++ kNN/kNN.py
@@ -0,0 +1,13 @@
+
+def classify0(inX, dataSet, labels, k):
+	dataCnt = dataSet.shape[0]
+	diffData = tile(inX, (dataCnt, 1)) - dataSet
+	sqrtData = diffData ** 2
+	sqDistance = sqrtData.sum(axis=1)
+	indexSort = sqDistance.argsort()
+	category = {}
+	for i in range(k):
+		 vLabel = labels[indexSort[i]]
+		 category[vLabel] = category.get(vLabel, 0) +1
+	sortedCat = sorted(category, key = operator.itemgetter(1), reverse = True)
+	return sortedCat[0][0]
